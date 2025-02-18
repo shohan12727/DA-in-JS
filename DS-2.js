f6f38d79dff5747a94ef77cb21ab5e70518e17d3
shohan12727 <aishohan001@gmail.com>
2025-02-18 13:22:08 +0600

    ds in js

---
+++ DS-2.js
@@ -0,0 +1,5 @@
+console.log ("ashraful islam shohan");
+console.log ("i am the man who face runs the palce");
+console.log ("i am from faridpur");
+console.log ("______________");
+console.log ("bangladesh");
