51772afff5cefeef98944d80c41b9e6ca17040f3
shohan12727 <aishohan001@gmail.com>
2025-02-18 14:11:38 +0600

    object

---
+++ ds_3_object.js
@@ -0,0 +1,11 @@
+const obj = {
+    name: 'ashraful islam shohan',
+    age : 20 ,
+    university : 'North south university',
+    stay_at : 'Basundhara R/A',
+    salary : 0 
+}
+
+console.log (obj.name,obj.age);
+
+console.log (obj['salary']);
